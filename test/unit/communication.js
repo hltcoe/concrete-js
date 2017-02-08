c5d7c8c6f7f9a7c582dbcadf9343ba87cae1a171
Craig Harman <craig@craigharman.net>
2017-02-08 12:32:36 -0500

    Added Karma+Jasmine config files for automated testing

---
+++ test/unit/communication.js
@@ -0,0 +1,26 @@
+describe("Communication unit tests", function() {
+  beforeEach(function() {
+    jasmine.getJSONFixtures().fixturesPath='base/test/mock';
+  });
+
+  it("communication.initFromTJSONProtocolObject()", function() {
+    var comm = new Communication();
+    var commJSONData = getJSONFixture('dog-bites-man.concrete.json');
+    comm.initFromTJSONProtocolObject(commJSONData);
+
+    expect(comm.id).toContain('dog-bites-man');
+  });
+
+  it("communication.addInternalReferences()", function() {
+    var comm = new Communication();
+    var commJSONData = getJSONFixture('dog-bites-man.concrete.json');
+    comm.initFromTJSONProtocolObject(commJSONData);
+    comm.addInternalReferences();
+
+    var firstSentence = comm.getFirstSentence();
+    var firstTokenization = comm.getFirstTokenization();
+
+    expect(firstTokenization.sentence).toEqual(firstSentence);
+    expect(firstSentence.section.communication).toEqual(comm);
+  });
+});

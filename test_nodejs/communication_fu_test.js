a7fb8437c554b81035ea05511c06432c4e2d575a
Chandler May <chandler.camille.may@gmail.com>
2023-10-17 00:33:28 +0000

    Check ref content.

--- test_nodejs/communication_fu_test.js
+++ test_nodejs/communication_fu_test.js
@@ -7,9 +7,8 @@ describe("addInternalReferences", function() {
   it("Returns Communication-level maps", function() {
     const comm = new concrete.communication.Communication();
     comm.initFromTJSONProtocolObject(commJSONData);
-    expect(
-      comm.addInternalReferences()
-    ).to.have.all.keys([
+    const refs = comm.addInternalReferences();
+    expect(refs).to.have.all.keys([
       "entityForUUID",
       "entityMentionForUUID",
       "sectionForUUID",
@@ -18,5 +17,32 @@ describe("addInternalReferences", function() {
       "situationMentionForUUID",
       "tokenizationForUUID"
     ]);
+    comm.sectionList.forEach((section) => {
+      expect(refs.sectionForUUID[section.uuid.uuidString]).to.equal(section);
+      section.sentenceList.forEach((sentence) => {
+        expect(refs.sentenceForUUID[sentence.uuid.uuidString]).to.equal(sentence);
+        expect(refs.tokenizationForUUID[sentence.tokenization.uuid.uuidString]).to.equal(sentence.tokenization);
+      });
+    });
+    comm.entitySetList.forEach((entitySet) =>
+      entitySet.entityList.forEach((entity) =>
+        expect(refs.entityForUUID[entity.uuid.uuidString]).to.equal(entity)
+      )
+    );
+    comm.entityMentionSetList.forEach((entityMentionSet) =>
+      entityMentionSet.mentionList.forEach((mention) =>
+        expect(refs.entityMentionForUUID[mention.uuid.uuidString]).to.equal(mention)
+      )
+    );
+    comm.situationSetList.forEach((situationSet) =>
+      situationSet.situationList.forEach((situation) =>
+        expect(refs.situationForUUID[situation.uuid.uuidString]).to.equal(situation)
+      )
+    );
+    comm.situationMentionSetList.forEach((situationMentionSet) =>
+      situationMentionSet.mentionList.forEach((mention) =>
+        expect(refs.situationMentionForUUID[mention.uuid.uuidString]).to.equal(mention)
+      )
+    );
   });
 });
